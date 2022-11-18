637c836e382d4405cce11fe25704768f22e9b0df
JN <jjn00712@gmail.com>
2022-11-18 16:02:44 +0000

    fix: Modify to apply to RE task

--- inference.py
+++ inference.py
@@ -1,46 +1,31 @@
+import os
+
+import numpy as np
 import pandas as pd
 import pytorch_lightning as pl
-import torch
 
-import train
-from data_loader.data_loaders import Dataloader
+import create_instance
+from utils import utils
 
 
 def inference(args, conf):
-    dataloader = Dataloader(
-        conf.model.model_name,
-        conf.train.batch_size,
-        conf.data.train_ratio,
-        conf.data.shuffle,
-        conf.path.train_path,
-        conf.path.test_path,
-        conf.path.predict_path,
-        conf.data.swap,
-    )
     trainer = pl.Trainer(gpus=1, max_epochs=conf.train.max_epoch, log_every_n_steps=1)
+    dataloader, model = create_instance.new_instance(conf)
+    model, _, __ = create_instance.load_model(args, conf, dataloader, model)
 
-    model, _, __ = train.load_model(args, conf, dataloader)
+    output = trainer.predict(model=model, datamodule=dataloader)
+    pred_answer, output_prob = zip(*output)
+    pred_answer = np.concatenate(pred_answer).tolist()
+    output_prob = np.concatenate(output_prob, axis=0).tolist()
+    pred_answer = utils.num_to_label(pred_answer)
 
-    model.eval()
-
-    predictions = trainer.predict(
-        model=model,
-        datamodule=dataloader,
+    output = pd.DataFrame(
+        {
+            "pred_label": pred_answer,
+            "probs": output_prob,
+        }
     )
-    trainer.test(model=model, datamodule=dataloader)
 
-    predictions = list(float(i) for i in torch.cat(predictions))
-    predictions_b = list(round(float(i), 1) for i in predictions)
-    predictions_n = [round(5 * x / (max(predictions) - min(predictions) + 1e-8), 1) for x in predictions]
-
-    output = pd.read_csv("../data/sample_submission.csv")
-    output_b = pd.read_csv("../data/sample_submission.csv")
-    output_n = pd.read_csv("../data/sample_submission.csv")
-
-    output["target"] = predictions
-    output_b["target"] = predictions_b
-    output_n["target"] = predictions_n
-
-    output.to_csv("output.csv", index=False)
-    output_b.to_csv("output_b.csv", index=False)
-    output_n.to_csv("output_n.csv", index=False)
+    if not os.path.isdir("prediction"):
+        os.mkdir("prediction")
+    output.to_csv("./prediction/submission.csv", index=False)
