f173773d98e0e10ff2a594efd75899e4ad6534c6
JN <jjn00712@gmail.com>
2022-11-25 06:06:38 +0000

    Feat: Add syllable tokenizer
    - config.yaml 파일에서 syllable 변수(bool)를 설정하여 syllable tokenizer를 적용할 수 있도록 구현
    	- + config.yaml 파일에서 max_len을 설정할 수 있도록 수정

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,24 +1,25 @@
 import os
 import pickle
 import re
+
 import pandas as pd
 import pytorch_lightning as pl
 import torch
 import transformers
-from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import KFold, StratifiedShuffleSplit, train_test_split
+from torch.utils.data import DataLoader, Dataset
 from tqdm.auto import tqdm
 
-class CustomDataset(Dataset):
 
+class CustomDataset(Dataset):
     def __init__(self, df):
         self.df = df
 
     def __getitem__(self, idx):
-        sentence = self.df['sentence'].iloc[idx]
-        subject_entity = self.df['subject_entity'].iloc[idx]
-        object_entity = self.df['object_entity'].iloc[idx]
-        label = self.df['label'].iloc[idx]
+        sentence = self.df["sentence"].iloc[idx]
+        subject_entity = self.df["subject_entity"].iloc[idx]
+        object_entity = self.df["object_entity"].iloc[idx]
+        label = self.df["label"].iloc[idx]
         return sentence, subject_entity, object_entity, label
 
     def __len__(self):
@@ -35,6 +36,8 @@ class BaseDataloader(pl.LightningDataModule):
         self.shuffle = config.dataloader.shuffle
         self.new_tokens = list(config.tokenizer.new_tokens)
         self.new_special_tokens = list(config.tokenizer.new_special_tokens)
+        self.max_length = config.tokenizer.max_len
+        self.use_syllable_tokenize = config.tokenizer.syllable
 
         self.train_path = config.path.train_path
         self.test_path = config.path.test_path
@@ -77,70 +80,144 @@ class BaseDataloader(pl.LightningDataModule):
         else:
             self.tokenizer = transformers.AutoTokenizer.from_pretrained(self.model_name)
 
-        # self.tokenizer.model_max_length = 256
         self.new_token_count = 0
         if self.new_tokens != []:
             self.new_token_count += self.tokenizer.add_tokens(self.new_tokens, special_tokens=False)
         if self.new_special_tokens != []:
-            self.new_token_count += self.tokenizer.add_tokens(self.new_special_tokens, special_tokens=True) 
-    
+            self.new_token_count += self.tokenizer.add_tokens(self.new_special_tokens, special_tokens=True)
+
     def batchify(self, batch):
-        ''' data collator '''
+        """data collator"""
         sentences, subject_entities, object_entities, labels = zip(*batch)
 
-        outs = self.tokenize(sentences, subject_entities, object_entities,)
-        input_ids = outs['input_ids']
-        token_type_ids = outs['token_type_ids']
-        attention_mask = outs['attention_mask']
+        outs = self.tokenize(
+            sentences,
+            subject_entities,
+            object_entities,
+        )
+        input_ids = outs["input_ids"]
+        token_type_ids = outs["token_type_ids"]
+        attention_mask = outs["attention_mask"]
         labels = torch.tensor(labels)
         return input_ids, token_type_ids, attention_mask, labels
 
     def tokenize(self, sentences, subject_entities, object_entities):
         """
-        tokenizer로 과제에 따라 tokenize 
+        tokenizer로 과제에 따라 tokenize
         """
         sep_token = self.tokenizer.special_tokens_map["sep_token"]
 
-        concat_entity = [e01 + sep_token + e02 for e01, e02 in zip(subject_entities, object_entities)]
-
-        tokens = self.tokenizer(
-            concat_entity,
-            sentences,
-            add_special_tokens=True,
-            padding="longest",
-            truncation=True,
-            return_tensors="pt",
-            max_length=256,
-        )
+        if self.use_syllable_tokenize:
+            entities = [[e01, e02] for e01, e02 in zip(subject_entities, object_entities)]
+            tokens = self.syllable_tokenizer(entities, sentences, self.max_length)
+        else:
+            concat_entity = [e01 + sep_token + e02 for e01, e02 in zip(subject_entities, object_entities)]
+            tokens = self.tokenizer(
+                concat_entity,
+                sentences,
+                add_special_tokens=True,
+                padding="longest",
+                truncation=True,
+                return_tensors="pt",
+                max_length=self.max_length,
+            )
 
         return tokens
 
+    def syllable_tokenizer(self, entities, sentences, max_seq_length):
+        input_ids_list = []
+        token_type_ids_list = []
+        attention_mask_list = []
+
+        sep_token_ids = self.tokenizer.convert_tokens_to_ids(self.tokenizer.special_tokens_map["sep_token"])
+        pad_token_ids = self.tokenizer.convert_tokens_to_ids(self.tokenizer.special_tokens_map["pad_token"])
+        cls_token_ids = self.tokenizer.convert_tokens_to_ids(self.tokenizer.special_tokens_map["cls_token"])
+
+        for entity, sentence in zip(entities, sentences):
+            now_index = 0
+            input_ids = [pad_token_ids] * (max_seq_length - 1)
+            attention_mask = [0] * (max_seq_length - 1)
+            token_type_ids = [0] * (max_seq_length - 1)
+
+            for e in entity:
+                pre_syllable = "_"
+                e = e.replace(" ", "_")
+                for syllable in e:
+                    if syllable == "_":
+                        pre_syllable = "_"
+                    if pre_syllable != "_":
+                        if syllable not in [",", "."]:
+                            syllable = "##" + syllable  # 중간 음절에는 모두 prefix를 붙입니다. (',', '.'에 대해서는 prefix를 붙이지 않습니다.)
+                        # 이순신은 조선 -> [이, ##순, ##신, ##은, 조, ##선]
+                    pre_syllable = syllable
+
+                    input_ids[now_index] = self.tokenizer.convert_tokens_to_ids(syllable)
+                    attention_mask[now_index] = 1
+                    now_index += 1
+
+                input_ids[now_index] = sep_token_ids
+                attention_mask[now_index] = 1
+                now_index += 1
+
+            sentence = sentence[: max_seq_length - 2 - now_index].replace(" ", "_")
+            pre_syllable = "_"
+            for syllable in sentence:
+                if syllable == "_":
+                    pre_syllable = syllable
+                if pre_syllable != "_":
+                    if syllable not in [",", "."]:
+                        syllable = "##" + syllable  # 중간 음절에는 모두 prefix를 붙입니다. (',', '.'에 대해서는 prefix를 붙이지 않습니다.)
+                    # 이순신은 조선 -> [이, ##순, ##신, ##은, 조, ##선]
+                pre_syllable = syllable
+
+                input_ids[now_index] = self.tokenizer.convert_tokens_to_ids(syllable)
+                attention_mask[now_index] = 1
+                token_type_ids[now_index] = 1
+                now_index += 1
+
+            input_ids = [cls_token_ids] + input_ids
+            input_ids[now_index + 1] = sep_token_ids
+            token_type_ids = [0] + token_type_ids
+            token_type_ids[now_index + 1] = 1
+            attention_mask = [1] + attention_mask
+            attention_mask[now_index + 1] = 1
+
+            input_ids_list.append(input_ids)
+            token_type_ids_list.append(token_type_ids)
+            attention_mask_list.append(attention_mask)
+
+        return {
+            "input_ids": torch.tensor(input_ids_list),
+            "token_type_ids": torch.tensor(token_type_ids_list),
+            "attention_mask": torch.tensor(attention_mask_list),
+        }
+
     def preprocess(self, df):
         from utils.utils import label_to_num
+
         """
         기존 subject_entity, object entity string에서 word만 추출
             e.g. "{'word': '비틀즈', 'start_idx': 24, 'end_idx': 26, 'type': 'ORG'}" => 비틀즈
         train/dev set의 경우 label을 str ->  int
         """
-        extract_entity = lambda row: eval(row)['word'].replace("'", "")
-        df['subject_entity'] = df['subject_entity'].apply(extract_entity)
-        df['object_entity'] = df['object_entity'].apply(extract_entity)
+        extract_entity = lambda row: eval(row)["word"].replace("'", "")
+        df["subject_entity"] = df["subject_entity"].apply(extract_entity)
+        df["object_entity"] = df["object_entity"].apply(extract_entity)
 
-        if isinstance(df['label'].iloc[0], str): 
-            num_labels = label_to_num(df['label'].values)
-            df['label'] = num_labels
-        
+        if isinstance(df["label"].iloc[0], str):
+            num_labels = label_to_num(df["label"].values)
+            df["label"] = num_labels
+
         return df
 
     def setup(self, stage="fit"):
         if stage == "fit":
             total_data = pd.read_csv(self.train_path)
 
-            if self.train_ratio < 1.0 :
+            if self.train_ratio < 1.0:
                 train_data, val_data = train_test_split(total_data, train_size=self.train_ratio)
 
-
-            # new dataframe 
+            # new dataframe
             train_df = self.preprocess(train_data)
             val_df = self.preprocess(val_data)
 
@@ -167,7 +244,7 @@ class BaseDataloader(pl.LightningDataModule):
 
     def predict_dataloader(self):
         return DataLoader(self.predict_dataset, batch_size=self.batch_size, collate_fn=self.batchify)
-    
+
     @property
     def new_vocab_size(self):
         return self.new_token_count + self.tokenizer.vocab_size
@@ -205,8 +282,8 @@ class KfoldDataloader(BaseDataloader):
     def prepare_data(self):
         self.total_data = pd.read_csv(self.train_path)
         kf = KFold(
-                n_splits=self.num_splits,
-                shuffle=self.shuffle,
+            n_splits=self.num_splits,
+            shuffle=self.shuffle,
         )
         self.split_indices = [s for s in kf.split(self.total_data)]
 
@@ -230,6 +307,7 @@ class KfoldDataloader(BaseDataloader):
 
             self.test_dataset = CustomDataset(test_inputs, test_targets)
             self.predict_dataset = CustomDataset(predict_inputs, predict_targets)
+
 
 class StratifiedDataloader(BaseDataloader):
     def __init__(
@@ -256,7 +334,7 @@ class StratifiedDataloader(BaseDataloader):
             new_special_tokens=new_special_tokens,
         )
 
-        assert self.train_ratio > 0.0 and self.train_ratio <1.0
+        assert self.train_ratio > 0.0 and self.train_ratio < 1.0
 
     def setup(self, stage="fit"):
         if stage == "fit":
@@ -266,8 +344,8 @@ class StratifiedDataloader(BaseDataloader):
             for train_idx, val_idx in split.split(total_data, total_data["label"]):
                 train_data = total_data.loc[train_idx]
                 val_data = total_data.loc[val_idx]
-                
-            # new dataframe 
+
+            # new dataframe
             train_df = self.preprocess(train_data)
             val_df = self.preprocess(val_data)
 
