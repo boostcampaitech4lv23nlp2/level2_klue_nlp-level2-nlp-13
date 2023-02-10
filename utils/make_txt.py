8d03399a9407c12c1ec26a33f2b9786c62a24aae
wjlee-ling <wjlee.ling@gmail.com>
2023-02-10 18:14:50 +0900

    [Fix] move make_txt into utils folder

--- utils/make_txt.py
+++ utils/make_txt.py
@@ -1,6 +1,6 @@
 import csv
-csv_file = './data/raw_data/test_data.csv'
-txt_file = './data/preprocessed_data/test.txt'
+csv_file = '../data/raw_data/test_data.csv'
+txt_file = '../data/preprocessed_data/test.txt'
 with open(txt_file, "w") as my_output_file:
     with open(csv_file, "r") as my_input_file:
         [my_output_file.write(row[1]+'\n') for row in csv.reader(my_input_file)]
